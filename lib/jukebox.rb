7ff2117c525a698b561e98b25e334e8208d1f75a
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 03:33:50 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,7 +11,12 @@ songs = [
 ]
 
 def help
-  puts "displays this help message"
+  prompt = "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+  puts prompt
 end
 
 def list(songs)
