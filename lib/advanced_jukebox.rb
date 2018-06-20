1e23fc9efec0012c8e6b93f2cf9f7497174db2a7
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-20 04:29:12 +0000

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -3,13 +3,13 @@
 #with the correct path to this directory on your computer
 
 # my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
+# "Go Go GO" => '/home/ryanamgung/jukebox-cli-nyc-web-062518/jukebox-cli/audio/Emerald-Park/01.mp3',
+# "LiberTeens" => '/home/ryanamgung/jukebox-cli-nyc-web-062518/jukebox-cli/audio/Emerald-Park/02.mp3',
+# "Hamburg" =>  '/home/ryanamgung/jukebox-cli-nyc-web-062518/jukebox-cli/audio/Emerald-Park/03.mp3',
+# "Guiding Light" => '/home/ryanamgung/jukebox-cli-nyc-web-062518/jukebox-cli/audio/Emerald-Park/04.mp3',
+# "Wolf" => '/home/ryanamgung/jukebox-cli-nyc-web-062518/jukebox-cli/audio/Emerald-Park/05.mp3',
+# "Blue" => '/home/ryanamgung/jukebox-cli-nyc-web-062518/jukebox-cli/audio/Emerald-Park/06.mp3',
+# "Graduation Failed" => '/home/ryanamgung/jukebox-cli-nyc-web-062518/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
 def help
--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -10,3 +10,50 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def help
+  prompt = "I accept the following commands:
+  - help : displays this help message
+  - list : displays a list of songs you can play
+  - play : lets you choose a song to play
+  - exit : exits this program"
+  puts prompt
+end
+
+def list(songs)
+  songs.each_with_index do |element, index|
+    puts "#{index}. #{element}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.chomp
+  songs.each_with_index do |song, index|
+    if input == song || input.to_i == index + 1
+      puts "Playing #{song}"
+      return
+    end
+  end
+  puts "Invalid input, please try again"
+end
+
+def exit_jukebox 
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+  prompt = nil
+  while prompt != "exit" do 
+      puts "Please enter a command:"
+      prompt = gets.chomp
+      if prompt == "list" 
+        list(songs)
+      elsif prompt == "play"
+        play(songs)
+      elsif prompt == "help"
+        help
+      end
+  end
+  exit_jukebox
+end
